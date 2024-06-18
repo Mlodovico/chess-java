3258225da289dbf86621578c90470d21b343f20e
mlodovico <murilo.lodovico@outlook.com>
2024-06-17 23:43:34 -0300

    feature: creating another parts of the board game and the relation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,12 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
 
-        Position pos = new Position(3, 6);
+        Board board = new Board(8, 8);
 
-        System.out.println(pos);
     }
 }
