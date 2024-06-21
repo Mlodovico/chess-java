ac68378c34fd1a4dcad8619186a733a55b7d2c23
mlodovico <murilo.lodovico@outlook.com>
2024-06-21 00:41:07 -0300

    feature: movement logic and rules to move rook piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -35,6 +35,8 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
 
+        validateTargetPosition(source, target);
+
         validateSourcePosition(source);
         Piece capturedPiece = makeMove(source, target);
 
@@ -55,6 +57,12 @@ public class ChessMatch {
         }
         if(!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There is no possible moves for the selected piece");
+        }
+    }
+
+    private void validateTargetPosition(Position source, Position target) {
+        if(!board.piece(source).possibleMove(target)) {
+            throw new ChessException("The chosen piece cant move to target position");
         }
     }
 
