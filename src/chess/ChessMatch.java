7cd3e25562d34a52bc45907b50049cae5bdaa7fc
mlodovico <murilo.lodovico@outlook.com>
2024-06-24 23:10:30 -0300

    feature: increased and decreased move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -157,7 +157,8 @@ public class ChessMatch {
     }
 
     private Piece makeMove(Position source, Position target) {
-        Piece movedPiece = board.removePiece(source);
+        ChessPiece movedPiece = (ChessPiece) board.removePiece(source);
+        movedPiece.increaseMoveCount();
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(movedPiece, target);
 
@@ -170,7 +171,8 @@ public class ChessMatch {
     };
 
     private void undoMove(Position source, Position target, Piece capturedPiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
 
         if (capturedPiece != null) {
